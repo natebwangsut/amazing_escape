fdc93f58037419f68d010c6c92440c84bfcfcf5c
Nate Bhurinat Wangsutthitham <nate.bwangsut@gmail.com>
2017-06-05 20:35:14 +1000

    Fix autostashing

--- core/src/mycontroller/EVController.java
+++ core/src/mycontroller/EVController.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package mycontroller;
 
 import controller.CarController;
@@ -73,128 +72,13 @@ public class EVController extends CarController {
         });
     }
 
-
     /**
      * Update the position based on the
      * @param delta
      */
     @Override
     public void update(float delta) {
-        pv.update(getView());
-
-        Action toDo = null;
-        toDo = backgroundState;
-
-        if (state == null || state.isCompleted()) {
-            state = null;
-
-            // fill dead end only if we are not working on a specific task and just following wall
-            int kuy;
-            if ((kuy = pv.fillDeadEnd(new Coordinate(getPosition()), getViewSquare())) > 0) {
-                logger.info("Filled in {} dead ends.", kuy);
-            }
-
-            FOVUtils.DeadEnd de;
-            if ((de=utils.deadEndAhead(pv))!=null) {
-                toDo = state = deh.getAction(getView(),de);
-            } else if (getView().get(getCoordinate()) instanceof MudTrap) {
-                toDo = state = th.getAction(getView(), "MudTrap");
-
-            } else if (getView().get(getCoordinate()) instanceof GrassTrap) {
-                toDo = state = th.getAction(getView(), "GrassTrap");
-            } else  {
-                toDo = backgroundState;
-            }
-
-        } else {
-            // Use the previous instruction
-            toDo = state;
-        }
-
-        // Not doing anything if null
-        if (toDo != null)
-            toDo.update(delta);
-    }
-
-
-    /**
-     * Get the coordinate position of the current car
-     *
-     * @return      car's coordinate
-     */
-    public Coordinate getCoordinate() {
-        String coordinates = getPosition();
-        Scanner scanner = new Scanner(coordinates);
-        scanner.useDelimiter(",");
-        int x = scanner.nextInt();
-        int y = scanner.nextInt();
-        return new Coordinate(x,y);
-    }
-
-}
-=======
-package mycontroller;
-
-import controller.CarController;
-import mycontroller.actions.Action;
-import mycontroller.actions.FollowAction;
-import mycontroller.actions.ReverseOutAction;
-import mycontroller.handler.DeadEndHandler;
-import mycontroller.handler.DiscreteTrapStrategy;
-import mycontroller.handler.IHandler;
-import tiles.GrassTrap;
-import tiles.LavaTrap;
-import tiles.MudTrap;
-import tiles.TrapTile;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import utilities.Coordinate;
-import world.Car;
-
-import java.util.LinkedList;
-import java.util.Scanner;
-
-public class EVController extends CarController {
-
-    private static Logger logger = LogManager.getLogger();
-
-    public FOVUtils utils;
-    private Action state = null;
-    private Action backgroundState = null;
-    private LinkedList<Action> aq;
-    private DeadEndHandler deh;
-    private DiscreteTrapStrategy th;
-    private PersistentView pv;
-
-    /**
-     * Constructor
-     *
-     * @param car
-     */
-    public EVController(Car car) {
-
-        super(car);  // uses CarController init()
-
-        utils = new FOVUtils(this);
-
-        deh = new DeadEndHandler(this);
-        th = new DiscreteTrapStrategy(this);
-
-        aq = new LinkedList<>();
-        this.aq.add(this.backgroundState);
-
-        pv = new PersistentView(this);
-        backgroundState = new FollowAction(this, c -> {
-            PersistentView.Property p = pv.get(c);
-            assert (p != null);
-            return p.logicalWall;
-        });
-    }
-
-    @Override
-    public void update(float delta) {
-      //  System.out.println("Current speed is: " + getVelocity());
+        //  System.out.println("Current speed is: " + getVelocity());
 
         pv.update(getView());
 
@@ -219,7 +103,7 @@ public class EVController extends CarController {
                 }*/
             } else if (getView().get(getCoordinate()) instanceof MudTrap) {
                 toDo = state = th.getAction(getView(), "MudTrap");
-                
+
             } else if (getView().get(getCoordinate()) instanceof GrassTrap){
                 toDo = state = th.getAction(getView(), "GrassTrap");
             } else  {
@@ -235,16 +119,19 @@ public class EVController extends CarController {
         if (toDo != null)
             toDo.update(delta);
     }
-    
+
+
+    /**
+     * Get the coordinate position of the current car
+     *
+     * @return      car's coordinate
+     */
     public Coordinate getCoordinate(){
         String coordinates = getPosition();
         Scanner scanner = new Scanner(coordinates);
         scanner.useDelimiter(",");
         int x = scanner.nextInt();
         int y = scanner.nextInt();
-        Coordinate coord = new Coordinate(x,y);
-        return coord;
+        return new Coordinate(x,y);
     }
-    
 }
->>>>>>> Stashed changes
--- core/src/mycontroller/PersistentView.java
+++ core/src/mycontroller/PersistentView.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package mycontroller;
 
 import controller.CarController;
@@ -6,7 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tiles.LavaTrap;
 import tiles.MapTile;
-import tiles.MudTrap;
+import tiles.TrapTile;
 import utilities.Coordinate;
 import world.WorldSpatial;
 
@@ -29,12 +28,16 @@ public class PersistentView {
 
     private static Logger logger = LogManager.getLogger();
 
-    final CarController con;
-    final FOVUtils utils;
-    protected Map<Coordinate, Property> masterView = new HashMap<>();
+    private final CarController con;
+    private final FOVUtils utils;
+
+    private Map<Coordinate, Property> masterView = new HashMap<>();
 
     private static final int    MAX_DE_GAP_SIZE     = 3;
     private static final float  R2_DONT_MESS        = 4;
+
+
+    private Coordinate lastDone = new Coordinate(-1,-1);
 
 
     /**
@@ -61,208 +64,6 @@ public class PersistentView {
         }
     }
 
-
-    /**
-     * Update the view based on the given map
-     * @param view
-     */
-    public void update(Map<Coordinate, MapTile> view) {
-        for (Map.Entry<Coordinate, MapTile> e : view.entrySet()) {
-            // skip tiles we already know
-            if (masterView.containsKey(e.getKey())) continue;
-
-            Property p = new Property();
-            p.tile = e.getValue();
-            p.coordinate = e.getKey();
-            // condition for our tile to be considered a logical wall
-            // if LavaTrap and MudTrap tiles are added as 'logical walls', the car may get stuck; not have any path to go through
-            p.logicalWall = FOVUtils.IS_WALL.test(e.getValue());// || e.getValue() instanceof LavaTrap || e.getValue() instanceof MudTrap;
-
-            masterView.put(e.getKey(), p);
-        }
-    }
-
-
-    /**
-     * Detects dead-ends and replace them with logical walls.
-     *
-     * @param start         coordinate of the starting detection coordinate
-     * @return              number of dead ends found
-     */
-    private int detectDeadEnd(Coordinate start) {
-
-        int dec = 0; // dead end count
-        Coordinate car = new Coordinate(con.getPosition());
-
-        // repeat for all directions
-        directional:
-        for (WorldSpatial.Direction dir : WorldSpatial.Direction.values()) {
-
-            /*
-            DED Algorithm: X = wall (real and virtual), R = road, ? = any
-
-            width=1     width=2     width=3
-            ? X ?       ? X X ?     ? X X X ?
-            X R X       X R R X     X R R R X
-
-            replace the road R here with a virtual wall ^ so above becomes:
-            ? X ?       ? X X ?     ? X X X ?
-            X X X       X X X X     X X X X X
-
-            Detection:
-                x=  -1 (0..n-1) n
-            y=1      ?    X     ?
-            y=0      X    R     X
-
-            And so we fill in all blocks at (x,y)=(0 to n-1, 0)
-
-            variable names
-            ?       c2      ?
-            cLeft   c1      c1 at last iteration
-             */
-
-            Coordinate cLeft = FOVUtils.directionalCoordinateAdd(start, new Coordinate(-1, 0), dir);
-            Property left = get(cLeft);
-            if (left == null || !left.logicalWall) continue directional;
-            int i;
-            width:
-            for (i = 0; i <= MAX_DE_GAP_SIZE; i++) {
-                Coordinate c = FOVUtils.directionalCoordinateAdd(start, new Coordinate(i, 0), dir);
-                // skip if our target is too close to car
-                if (FOVUtils.dist2(car, c) < R2_DONT_MESS) continue directional;
-
-                Property p = get(c);
-                Coordinate c2 = FOVUtils.directionalCoordinateAdd(start, new Coordinate(i, 1), dir);
-                Property p2 = get(c2);
-
-                if (p == null || p2 == null) continue directional;
-
-                /*
-                 * As stated above, we require a logical wall right above a road
-                 * and the road must also only be a road and not a logical wall
-                 */
-                if (p.tile.getName().equals("Road") && !p.logicalWall && p2.logicalWall) continue width;
-
-                // we found the end (cRight)
-                if (p.logicalWall) break;
-
-                continue directional;
-            }
-
-            // check if in range
-            if (i > 0 && i <= MAX_DE_GAP_SIZE) {
-                logger.info("Filling ");
-                for (int j = 0; j < i; j++) {
-                    // mark all roads in here as logical walls
-                    Coordinate c = FOVUtils.directionalCoordinateAdd(start, new Coordinate(j, 0), dir);
-                    Property p = get(c);
-                    p.logicalWall = true;
-                    logger.info("[{}]", c);
-                }
-                dec++;
-            }
-        }
-        return dec;
-    }
-
-
-    /**
-     * Can the deadend be fill ?
-     *
-     * @param cen
-     * @param limit
-     * @return
-     */
-    public int fillDeadEnd(Coordinate cen, int limit) {
-        // I will not fill dead ends when car is currently in a virtual wall
-        Property p = get(new Coordinate(con.getPosition()));
-        assert(p!=null);
-        if (p.logicalWall && !p.tile.getName().equals("Wall")) return 0;
-
-        int totalDef = 0;
-        // indicates the number of dead ends filled in a single loop
-        int def;
-        // repeatedly fill in dead end until no more is fillable
-        do {
-            def = 0;
-            // fills starting with tile within center +- limit
-            Coordinate c = new Coordinate(cen.x - limit, cen.y - limit);
-            for (; c.x <= cen.x + limit; c.x++)
-                for (c.y = cen.y - limit; c.y <= cen.y + limit; c.y++) {
-                    def += detectDeadEnd(c);
-                }
-            totalDef += def;
-        } while (def > 0);
-        return totalDef;
-    }
-
-
-    /**
-     * Return the property given the coordinate
-     *
-     * @param c     coordinate to show the property
-     * @return      property
-     */
-    public Property get(Coordinate c) {
-        return masterView.get(c);
-    }
-}
-=======
-package mycontroller;
-
-import controller.CarController;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import tiles.LavaTrap;
-import tiles.MapTile;
-import tiles.MudTrap;
-import tiles.TrapTile;
-import utilities.Coordinate;
-import world.WorldSpatial;
-
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Created by Kolatat on 1/6/17.
- */
-public class PersistentView {
-
-    private static Logger logger = LogManager.getLogger();
-
-    final CarController con;
-    final FOVUtils utils;
-    protected Map<Coordinate, Property> masterView = new HashMap<>();
-
-    public PersistentView(CarController con) {
-        this.con = con;
-        if (con instanceof EVController) {
-            utils = ((EVController) con).utils;
-        } else {
-            utils = new FOVUtils(con);
-        }
-    }
-
-    public void update(Map<Coordinate, MapTile> view) {
-        for (Map.Entry<Coordinate, MapTile> e : view.entrySet()) {
-            // skip tiles we already know
-            if (masterView.containsKey(e.getKey())) continue;
-
-            Property p = new Property();
-            p.tile = e.getValue();
-            p.coordinate = e.getKey();
-            // condition for our tile to be considered a logical wall
-                // if LavaTrap and MudTrap tiles are added as 'logical walls', the car may get stuck; not have any path to go through
-            p.logicalWall = FOVUtils.IS_WALL.test(e.getValue());// || e.getValue() instanceof LavaTrap || e.getValue() instanceof MudTrap;
-
-            masterView.put(e.getKey(), p);
-        }
-    }
-
-    private static final int MAX_DE_GAP_SIZE = 3;
-    private static final float R2_DONT_MESS = 4;
-
-    Coordinate lastDone = new Coordinate(-1,-1);
 
     private void dumbTrapDetect(Coordinate start, int lim){
         Coordinate c, cs[];
@@ -356,6 +157,28 @@ public class PersistentView {
         }
     }
 
+
+    /**
+     * Update the view based on the given map
+     *
+     * @param view
+     */
+    public void update(Map<Coordinate, MapTile> view) {
+        for (Map.Entry<Coordinate, MapTile> e : view.entrySet()) {
+            // skip tiles we already know
+            if (masterView.containsKey(e.getKey())) continue;
+
+            Property p = new Property();
+            p.tile = e.getValue();
+            p.coordinate = e.getKey();
+            // condition for our tile to be considered a logical wall
+            // if LavaTrap and MudTrap tiles are added as 'logical walls', the car may get stuck; not have any path to go through
+            p.logicalWall = FOVUtils.IS_WALL.test(e.getValue());// || e.getValue() instanceof LavaTrap || e.getValue() instanceof MudTrap;
+
+            masterView.put(e.getKey(), p);
+        }
+    }
+
     private boolean dtdl(Coordinate start, Class<? extends TrapTile> type, int x, int y){
         Coordinate rs = new Coordinate(start.x-x, start.y-y);
         if(get(rs) != null && get(rs).logicalWall) return false;
@@ -363,10 +186,12 @@ public class PersistentView {
         return get(rs)==null||!get(rs).logicalWall;
     }
 
-    /*
+
+    /**
      * Detects dead-ends and replace them with logical walls.
      *
-     * @return number of dead-ends replaced
+     * @param start         coordinate of the starting detection coordinate
+     * @return              number of dead ends found
      */
     protected int detectDeadEnd(Coordinate start) {
         int dec = 0; // dead end count
@@ -442,6 +267,13 @@ public class PersistentView {
         return dec;
     }
 
+    /**
+     * Can the deadend be fill ?
+     *
+     * @param cen
+     * @param limit
+     * @return
+     */
     public int fillDeadEnd(Coordinate cen, int limit) {
         if(cen==lastDone) return 0;
         lastDone = new Coordinate(cen.x, cen.y);
@@ -456,7 +288,7 @@ public class PersistentView {
         int totalDef = 0;
         // indicates the number of dead ends filled in a single loop
         int def;
-        // repeatedly fill in dead end until no more is fillable
+        // repeatedly fill in dead end until no more is fill-able
         do {
             def = 0;
             // fills starting with tile within center +- limit
@@ -470,14 +302,14 @@ public class PersistentView {
         return totalDef;
     }
 
+
+    /**
+     * Return the property given the coordinate
+     *
+     * @param c     coordinate to show the property
+     * @return      property
+     */
     public Property get(Coordinate c) {
         return masterView.get(c);
     }
-
-    public static class Property {
-        MapTile tile;
-        Coordinate coordinate;
-        boolean logicalWall;
-    }
 }
->>>>>>> Stashed changes
